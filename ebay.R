a0af0d4c68c145fbab61f9e30527a00d1525863f
Anuj Panday <Anuj Panday>
2017-01-17 00:12:02 -0500

    got item names figured out

--- ebay.R
+++ ebay.R
@@ -1,1 +1,6 @@
-initial commit yo
+library(rvest)
+html <- read_html("http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Complete=1&LH_Sold=1&_nkw=2016+bowman+chrome&_ipg=200&rt=nc")
+items<-html_nodes(html, "h3.lvtitle")
+
+items<-html_text(items)
+items[6]
