9c1037baf449549d72d8072f51fbc60acefa0165
Panday <apanday@cardlytics.com>
2017-02-20 22:47:10 -0500

    working copy of scraper

--- ebay.R
+++ ebay.R
@@ -1,6 +1,48 @@
 library(rvest)
-html <- read_html("http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Complete=1&LH_Sold=1&_nkw=2016+bowman+chrome&_ipg=200&rt=nc")
-items<-html_nodes(html, "h3.lvtitle")
+library(taskscheduleR)
 
+## access old data
+setwd("C:/Users/anujp_000/Dropbox/R Directory/Ebay/ebay")
+
+searchterm<-'2016 Bowman Chrome'
+updateFile<-'Bowman.csv'
+
+
+old<-read.csv(updateFile)
+old<-old[,c(2:4)]
+
+
+##get data and append to old data
+searchSold<-function(term){
+
+newterm<-gsub(' ','%20',term)
+
+url<-paste0('http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=', newterm, '&LH_Complete=1&LH_Sold=1&rt=nc&_trksid=p2045573.m1684')
+
+html<-read_html(url)
+
+items<-html_nodes(html, "h3.lvtitle")
 items<-html_text(items)
-items[6]
+
+prices<-html_nodes(html, ".bidsold")
+prices<-html_text(prices)
+
+soldwhen<-html_nodes(html, ".tme")
+soldwhen<-html_text(soldwhen)
+
+df <- data.frame(items, prices, soldwhen)
+
+df<-sapply(df, function(x) gsub("[\r\n\t]", "", x) )
+
+df<-data.frame(df)
+
+return (df)
+}
+
+new<-searchSold(searchterm)
+
+
+full<-rbind(new, old)
+full<-unique(full)
+
+write.csv(full, updateFile)
